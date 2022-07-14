c6019ef1aa4d09ec8288ae8097ecb337a717e1cb
carloss-ziegler <chziegler445@gmail.com>
2022-07-14 00:52:29 -0300

    first commit

---
+++ src/data/categories.ts
@@ -0,0 +1,7 @@
+import {Category} from "../types/Category"
+
+export const categories: Category = {
+    food: { title: 'Alimentação', color: '#1C94FC', expense: true },
+    rent: { title: 'Aluguel', color: '#7C24CC', expense: true },
+    salary: { title: 'Salário', color: '#048444', expense: false }
+}
